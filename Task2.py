1467d134648936f2aece1a32778e156c7097371d
Ahamed098 <phatanahamedali890@gmail.com>
2025-09-18 20:14:41 +0530

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,18 @@
+# Task 2: Create a Personalized Greeting
+
+# Taking first name and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenating full name
+full_name = first_name + " " + last_name
+
+# Displaying greeting
+print(f"\nHello, {full_name}! Welcome to the Python world 🚀")
+
+
+output:
+Enter your first name: Ahamed
+Enter your last name: Khan
+
+Hello, Ahamed Khan! Welcome to the Python world 🚀
